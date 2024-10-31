7b36b9167d39a0893a47487d8a703497ee373ec6
Urian666 <Urian4799@gmail.com>
2024-10-31 23:30:02 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict = {"Alex": 2001,"Ded": 1940}
+print(my_dict)
+print(my_dict["Ded"])
+print(my_dict.get("Anton"))
+my_dict ["Nik"] = 2002
+my_dict ["Don"] = 2003
+print(my_dict.pop("Alex"))
+print(my_dict)
+
+
+my_set = {5, 2.34, "World", 5, 3, 2.34}
+print(my_set)
+my_set.add(2)
+my_set.add(45)
+print(my_set.discard(2))
+print(my_set)
